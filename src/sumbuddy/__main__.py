e45effcd9e88f02190ca6b923af3d72a6c95b24a
Matthew Thompson <thompson.4509@osu.edu>
2024-07-29 14:11:20 -0400

    Update input variable name to represent file or dir as input

--- src/sumbuddy/__main__.py
+++ src/sumbuddy/__main__.py
@@ -9,13 +9,13 @@ from tqdm import tqdm
 import sys
 import os
 
-def get_checksums(input_directory, output_filepath=None, ignore_file=None, include_hidden=False, algorithm='md5', length=None):
+def get_checksums(input_path, output_filepath=None, ignore_file=None, include_hidden=False, algorithm='md5', length=None):
     """
     Generate a CSV file with the filepath, filename, and checksum of all files in the input directory according to patterns to ignore. Checksum column is labeled by the selected algorithm (e.g., 'md5' or 'sha256').
     
     Parameters:
     ------------
-    input_directory - String. Directory to traverse for files.
+    input_path - String. File or directory to traverse for files.
     output_filepath - String [optional]. Filepath for the output CSV file. Defaults to None, i.e. output will be to stdout.
     ignore_file - String [optional]. Filepath for the ignore patterns file.
     include_hidden - Boolean [optional]. Whether to include hidden files. Default is False.
@@ -24,11 +24,11 @@ def get_checksums(input_directory, output_filepath=None, ignore_file=None, inclu
     """
     mapper = Mapper()
 
-    if os.path.isfile(input_directory):
-        file_paths = [input_directory]
+    if os.path.isfile(input_path):
+        file_paths = [input_path]
     else:
         try:
-            file_paths = mapper.gather_file_paths(input_directory, ignore_file=ignore_file, include_hidden=include_hidden)
+            file_paths = mapper.gather_file_paths(input_path, ignore_file=ignore_file, include_hidden=include_hidden)
         except (EmptyInputDirectoryError, NoFilesAfterFilteringError) as e:
             sys.exit(str(e))
 
@@ -45,7 +45,7 @@ def get_checksums(input_directory, output_filepath=None, ignore_file=None, inclu
         writer.writerow(["filepath", "filename", f"{algorithm}"])
 
         disable_tqdm = output_filepath is None
-        for file_path in tqdm(file_paths, desc=f"Calculating {algorithm} checksums on {input_directory}", disable=disable_tqdm):
+        for file_path in tqdm(file_paths, desc=f"Calculating {algorithm} checksums on {input_path}", disable=disable_tqdm):
             checksum = hasher.checksum_file(file_path, algorithm=algorithm, length=length)
             writer.writerow([file_path, os.path.basename(file_path), checksum])
 
@@ -54,14 +54,14 @@ def get_checksums(input_directory, output_filepath=None, ignore_file=None, inclu
             output_stream.close()
 
     if output_filepath:     
-        print(f"{algorithm} checksums for {input_directory} written to {output_filepath}")
+        print(f"{algorithm} checksums for {input_path} written to {output_filepath}")
 
 def main():
 
     available_algorithms = ', '.join(hashlib.algorithms_available)
         
     parser = argparse.ArgumentParser(description="Generate CSV with filepath, filename, and checksums for all files in a given directory")
-    parser.add_argument("input_dir", help="Directory to traverse for files")
+    parser.add_argument("input_path", help="File or directory to traverse for files")
     parser.add_argument("-o", "--output-file", help="Filepath for the output CSV file; defaults to stdout", default=None)
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-i", "--ignore-file", help="Filepath for the ignore patterns file")
@@ -80,7 +80,7 @@ def main():
             sys.exit("Exited without executing")
         
     try:
-        get_checksums(args.input_dir, args.output_file, args.ignore_file, args.include_hidden, args.algorithm, args.length)
+        get_checksums(args.input_path, args.output_file, args.ignore_file, args.include_hidden, args.algorithm, args.length)
     except (LengthUsedForFixedLengthHashError) as e:
         sys.exit(str(e))
 
