809e179cdd173d47149b7630fc05857da2b429ef
Matthew Thompson <thompson.4509@osu.edu>
2024-07-29 14:49:34 -0400

    Ensure input to gather_file_paths is a directory
    It does not make sense to use this function on a single file

--- src/sumbuddy/exceptions.py
+++ src/sumbuddy/exceptions.py
@@ -3,6 +3,11 @@ class EmptyInputDirectoryError(Exception):
         message = f"The directory {input_directory} and subdirectories (if any) contain no files. \nPlease provide a directory with files."
         super().__init__(message)
 
+class NotADirectoryError(Exception):
+    def __init__(self, input_directory):
+        message = f"The input path '{input_directory}' is not a directory. \nPlease provide a directory with files."
+        super().__init__(message)
+
 class NoFilesAfterFilteringError(Exception):
     def __init__(self, input_directory, ignore_file):
         message = f"The directory {input_directory} contains files, but all are filtered out. \nCheck patterns in your {ignore_file} file and/or hidden files settings."
