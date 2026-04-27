cadf1d2f6938174b9d5b3bedd2517305a05ea028
Matthew Thompson <thompson.4509@osu.edu>
2026-04-27 12:06:42 -0400

    Add version printing command to CLI and package via __version__

--- src/sumbuddy/__init__.py
+++ src/sumbuddy/__init__.py
@@ -1,3 +1,4 @@
+from sumbuddy.__about__ import __version__
 from sumbuddy.__main__ import get_checksums
 from sumbuddy.mapper import Mapper
 from sumbuddy.hasher import Hasher
@@ -10,4 +11,4 @@ hasher_instance = Hasher()
 gather_file_paths = mapper_instance.gather_file_paths
 checksum_file = hasher_instance.checksum_file
 
-__all__ = ["checksum_file", "get_checksums", "gather_file_paths"]
+__all__ = ["__version__", "checksum_file", "get_checksums", "gather_file_paths"]
